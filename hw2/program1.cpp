a023e93037079bb9eb34bbe5e6d7e23efa4515bd
dasqwaer <dasqwaer@gmail.com>
2021-03-02 21:57:06 +0700

    Create program1.cpp, program1. Task2

---
+++ hw2/program1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Создать перечисление с возможными вариантами символов для игры в крестики нолики
+
+enum class TTT_simbol {
+	X,
+	O,
+	Empty
+};
+
+int main (){
+		
+    
+   
+return 0;
+}
