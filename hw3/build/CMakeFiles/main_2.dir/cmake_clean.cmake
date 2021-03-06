ee83ee471af1d2fbd1b7b39b91a13eaeee71370f
dasqwaer <dasqwaer@gmail.com>
2021-03-06 21:47:59 +0700

    Compled task 2 vs homework 3. Create main_2, pr2.cpp

---
+++ hw3/build/CMakeFiles/main_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_2.dir/pr2.cpp.o"
+  "main_2"
+  "main_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
